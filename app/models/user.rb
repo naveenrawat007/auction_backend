60f5cb2db6abfe03bb7b6cc8b589875c4ddecd32
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-15 14:11:30 +0530

    serialize stmnt removed from json field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  serialize :type_attributes, JSON
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
