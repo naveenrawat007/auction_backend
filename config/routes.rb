1320de5954c945e072237cce170bf203d7456172
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-15 14:10:28 +0530

    check the token route added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1', constraints: ApiConstraint.new(version: 1) do
       devise_for :users, controllers:{ sessions: 'api/v1/users/sessions', registrations: 'api/v1/users/registrations' }
+      get 'users/check_token', to: 'users#check'
     end
   end
 end
