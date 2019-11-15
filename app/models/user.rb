9113600a739abe68564b3adcfb5a6504747d7861
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-15 12:01:31 +0530

    user model side type_attributes serialize

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  serialize :type_attributes, JSON
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
