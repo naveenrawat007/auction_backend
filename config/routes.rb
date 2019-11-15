6b0f0b819a554a48921d314b98a35cfba9f50c9c
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-15 11:22:57 +0530

    routes added for login signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1', constraints: ApiConstraint.new(version: 1) do
+      devise_for :users, controllers:{ sessions: 'api/v1/users/sessions', registrations: 'api/v1/users/registrations' }
+    end
+  end
 end
