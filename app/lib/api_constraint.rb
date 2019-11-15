ebd01024699f34b3d56eb8327472b35ad3783746
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-15 11:26:21 +0530

    api constraint class added

---
+++ app/lib/api_constraint.rb
@@ -0,0 +1,10 @@
+class ApiConstraint
+	def initialize(options)
+		@version = options[:version]
+		@default = options[:default]
+	end
+
+	def matches?(req)
+		@default || req.headers['Accept'].include?("application/vnd.auction_backend.v#{@version}") && (req.headers['Content-Type'].include?("application/json") || req.headers['Content-Type'].include?("multipart/form-data"))
+	end
+end
