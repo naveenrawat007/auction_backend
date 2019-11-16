690e6590cd2c60e5ec158d4136758d4973a350e2
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-16 11:35:46 +0530

    forgot password and new_password actions added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,26 @@
 module Api
   module V1
     class UsersController < MainController
-      before_action :authorize_request
+      before_action :authorize_request, except: [:forgot_password]
       def check
         render json: {message: "Authenticated", status: 100}, status: 200
+      end
+      def forgot_password
+        @user = User.find_by(email: params[:email])
+        if @user
+          Sidekiq::Client.enqueue_to_in("default", Time.now, UserPasswordWorker, @user.id)
+          render json: { message: "An Email to reset password is sent to your email.", status: 200}, status: 200
+        else
+          render json: { message: "No Account Infomation found with this account.", error: "Account not found", status: 404}, status: 200
+        end
+      end
+      def new_password
+        @current_user.password = params[:user][:password]
+        if @current_user.save
+          render json: { message: "Password changed sucessfully. Now you can login.", status: 200}, status: 200
+        else
+          render json: { message: "Could not change password.", error: "Password error.", status: 400}, status: 200
+        end
       end
     end
   end
