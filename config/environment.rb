1afc80503a40e4e25f23bb15869f48da3f95cc14
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-19 18:58:54 +0530

    app config file added

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
 # Initialize the Rails application.
 Rails.application.initialize!
