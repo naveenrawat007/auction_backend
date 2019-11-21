728ccc33458bc20874a638c9d46562bf9e9fd36c
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-21 18:12:15 +0530

    routes added for users show and users profile update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,9 @@ module Api
   module V1
     class UsersController < MainController
       before_action :authorize_request, except: [:forgot_password]
+      def show
+        render json: {user: UserSerializer.new(@current_user, root: false, serializer_options: {token: @current_user.auth_token}), status:200}, status:200
+      end
       def check
         render json: {user: UserSerializer.new(@current_user, root: false, serializer_options: {token: @current_user.auth_token}), message: "Authenticated", status: 100}, status: 200
       end
@@ -40,6 +43,48 @@ module Api
       def resend_verification_code
         ConfirmationSender.send_confirmation_to(@current_user)
         render json: {message: "An email containing verification code is sent successfully.", status: 208}, status: 200
+      end
+      def update_profile
+        if params[:user][:email].blank? == false || params[:user][:phone].blank? == false
+          old_user = User.find_by(email: params[:user][:email])
+          old_user ||= User.find_by(email: params[:user][:phone_number])
+          if old_user
+            if old_user.id == @current_user.id || !old_user
+              if @current_user.update(update_params)
+                if params[:user][:type_attributes].blank? == false
+                  @current_user.type_attributes = type_attributes_permitter
+                  @current_user.save
+                end
+                render json: {user: UserSerializer.new(@current_user, root: false, serializer_options: {token: @current_user.auth_token}),message: "Profile updated successfully", status:200} and return
+              else
+                render json: {message: "Could not update", status: 400} and return
+              end
+            else
+              render json: {message: "Email or Phone number already exist.", status: 406} and return
+            end
+          else
+            if @current_user.update(update_params)
+              if params[:user][:type_attributes].blank? == false
+                @current_user.type_attributes = type_attributes_permitter
+                @current_user.save
+              end
+              render json: {user: UserSerializer.new(current_user, root: false, serializer_options: {token: @current_user.auth_token}),message: "Profile updated successfully", status:200} and return
+            else
+              render json: {message: "Could not update", status: 406} and return
+            end
+          end
+        else
+          render json: {message: "Email and Phone can't be blank.", status: 406} and return
+        end
+      end
+
+      private
+      def update_params
+        params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :company_name, :mobile, :address, :city, :state, :realtor_licence, :broker_licence, :type_attributes)
+      end
+
+      def type_attributes_permitter
+        JSON.parse(params[:user][:type_attributes].to_json)
       end
     end
   end
