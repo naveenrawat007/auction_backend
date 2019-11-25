5a57847cbfebc92d5943632face8a7524a733bfe
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-25 19:15:40 +0530

    photo model added

---
+++ app/models/photo.rb
@@ -0,0 +1,6 @@
+class Photo < ApplicationRecord
+  has_attached_file :image, styles: { thumb: ["100x100#", :jpg] }
+  validates_attachment_size :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  belongs_to :imageable, polymorphic: true
+end
