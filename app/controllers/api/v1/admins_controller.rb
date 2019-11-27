e94fc5b84f023ef5b1017779f0b705314e532aa7
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-27 19:13:35 +0530

    paginated reponse returned

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -4,11 +4,11 @@ module Api
       before_action :authorize_request
       def users_list
         if params[:search_str].blank? == false
-          @users = User.where("lower(first_name) LIKE :search OR lower(last_name) LIKE :search OR lower(email) LIKE :search ", search: "%#{params[:search_str].downcase}%").order(created_at: :desc)
+          @users = User.where("lower(first_name) LIKE :search OR lower(last_name) LIKE :search OR lower(email) LIKE :search ", search: "%#{params[:search_str].downcase}%").order(created_at: :desc).paginate(page: params[:page], per_page: 10)
         else
-          @users = User.all.order(created_at: :desc)
+          @users = User.all.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
         end
-        render json: {users: ActiveModelSerializers::SerializableResource.new(@users, each_serializer: UserSerializer), status: 200 }
+        render json: {users: ActiveModelSerializers::SerializableResource.new(@users, each_serializer: UserSerializer), status: 200, meta: {current_page: @users.current_page, total_pages: @users.total_pages} }
       end
     end
   end
