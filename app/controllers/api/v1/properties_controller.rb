b6bbb8007437fe452642fa009ec9e9a95ed3bcfd
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-28 17:06:29 +0530

    properties create action added

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -9,7 +9,22 @@ module Api
       end
 
       def create
-        @property = @current_user.
+        @property = @current_user.owned_properties.new(property_params)
+        if @property.save
+          if params[:property][:images].blank? == false
+            params[:property][:images].each do |image|
+              @property.photos.create(image: image)
+            end
+          end
+          render json: {message: "Property added sucessfully.", status: 200}, status: 200
+        else
+          render json: {message: "Property could not be added.", status: 400}, status: 200
+        end
+      end
+
+      private
+      def property_params
+      params.require(:property).permit(:address, :category, :type, :bedrooms, :bathrooms, :garage, :area, :lot_size, :year_built, :units, :price_per_sq_ft, :headliner, :ms_available, :flooded, :flood_count, :estimated_rehab_cost, :description, :seller_price, :buy_now_price, :auction_started_at, :auction_length, :auction_ending_at, :status)
       end
     end
   end
