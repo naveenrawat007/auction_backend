275847c136521cc620029db69477aa00ba6a37fc
rajan-gith <rajan.prajapati@berylsystems.com>
2019-12-03 22:18:53 +0530

    properties controller updated

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -32,6 +32,25 @@ module Api
       def update
         @property = Property.find_by(id: params[:property][:id])
         if @property.update(property_update_params)
+          @property.deal_analysis_type = params[:property][:deal_analysis_type]
+          @property.after_rehab_value = params[:property][:after_rehab_value]
+          @property.asking_price = params[:property][:asking_price]
+          @property.estimated_rehab_cost = params[:property][:estimated_rehab_cost]
+          @property.arv_analysis = params[:property][:arv_analysis]
+          @property.description_of_repairs = params[:property][:description_of_repairs]
+          @property.estimated_rehab_cost_attr = estimated_rehab_cost_attributes_permitter
+          if @property.deal_analysis_type == "Rehab & Flip Deal"
+            @property.profit_potential = params[:property][:profit_potential]
+          elsif @property.deal_analysis_type == "Landlord deal"
+            if @property.landlord_deal
+              @landlord_deal = @property.build_landlord_deal
+              @landlord_deal.save
+            else
+              @landlord_deal = @property.landlord_deal
+            end
+            @landlord_deal.update(landlord_deal_params)
+            @landlord_deal.save
+          end
           render json: {property: PropertySerializer.new(@property), message: "Property updated sucessfully.", status: 200}, status: 200
         else
           render json: {property: PropertySerializer.new(@property), message: "Property could not be updated.", status: 400}, status: 200
@@ -39,11 +58,11 @@ module Api
       end
       private
       def property_params
-      params.require(:property).permit(:address, :city, :state, :zip_code, :category, :p_type, :headliner, :mls_available, :flooded, :flood_count, :description)
+        params.require(:property).permit(:address, :city, :state, :zip_code, :category, :p_type, :headliner, :mls_available, :flooded, :flood_count, :description)
       end
 
       def property_update_params
-        params.require(:property).permit(:address, :city, :state, :zip_code, :category, :p_type, :headliner, :mls_available, :flooded, :flood_count, :description)
+        params.require(:property).permit(:address, :city, :state, :zip_code, :category, :p_type, :headliner, :mls_available, :flooded, :flood_count, :description, :seller_price, :buy_now_price, :auction_started_at, :auction_length, :auction_ending_at, :title_status, :youtube_url)
       end
 
       def residential_type_attributes_permitter
@@ -54,6 +73,12 @@ module Api
       end
       def land_type_attributes_permitter
         JSON.parse(params[:property][:land_attributes].to_json)
+      end
+      def estimated_rehab_cost_attributes_permitter
+        JSON.parse(params[:property][:estimated_rehab_cost_attr].to_json)
+      end
+      def landlord_deal_params
+        params.require(:property).permit(:closing_cost, :short_term_financing_cost, :total_acquisition_cost, :taxes_annually, :insurance_annually, :amount_financed_percentage, :amount_financed, :interest_rate, :loan_terms, :principal_interest, :taxes_monthly, :insurance_monthly, :piti_monthly_debt, :monthly_rent, :total_gross_yearly_income, :vacancy_rate, :adjusted_gross_yearly_income, :est_annual_management_fees, :est_annual_operating_fees, :annual_debt, :net_operating_income, :annual_cash_flow, :monthly_cash_flow, :total_out_of_pocket, :roi_cash_percentage)
       end
     end
   end
