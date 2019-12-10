f645c3aef016a348c00e2fa3d4e535a30f007e94
rajan-gith <rajan.prajapati@berylsystems.com>
2019-12-10 19:20:50 +0530

    under review property serializer added

---
+++ app/serializers/under_review_property_serializer.rb
@@ -0,0 +1,32 @@
+class UnderReviewPropertySerializer < ActiveModel::Serializer
+  def attributes(*args)
+    data = super
+    data[:created_at] = object.created_at.strftime("%B %e, %Y")
+    data[:updated_at] = object.updated_at.strftime("%B %e, %Y")
+    data[:id] = object.id
+    data[:first_name] = object.owner.first_name
+    data[:last_name] = object.owner.last_name
+    data[:user_type] = user_type
+    data[:address] = object.address
+    data[:submitted_date] = object.created_at.strftime("%b %e, %Y")
+    data[:auction_started_at] = object.auction_started_at ? object.auction_started_at.strftime("%b %e, %Y") : ""
+    data[:auction_length] = object.auction_length
+    data
+  end
+
+  def user_type
+    u_type = ""
+    object.owner.type_attributes.each do |type|
+      if type[1] == true
+        u_type = type[0]
+        break
+      end
+    end
+    if u_type.include?("inves")
+      "Investor"
+    else
+      "Realtor"
+    end
+  end
+
+end
