779b45b0a571e49eb89b997eb296a751c166bd74
rajan-gith <rajan.prajapati@berylsystems.com>
2019-12-16 15:25:31 +0530

    Properties controller changes

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -126,6 +126,98 @@ module Api
         end
       end
       def register
+        if params[:submit_type] == "register"
+          @old_user = User.find_by(email: params[:user][:email])
+          @old_user ||= User.find_by(phone_number: params[:user][:phone_number])
+          if !@old_user
+            @user = User.new(configure_sign_up_params)
+            ConfirmationSender.send_confirmation_to(@user)
+            if @user.save
+              token = JsonWebToken.encode(user_id: @user.id)
+              @user.auth_token = token
+              @user.save
+            else
+              render json: { message: "Can not add user.", error: "User save error", status: 400}, status: 200 and return
+            end
+          else
+            render json: { message: "User already exist with this email or phone number.", error: "User exists", status: 409}, status: 200 and return
+          end
+        elsif params[:submit_type] == "login"
+          @user = User.find_by(email:params[:user][:email])
+          if @user
+            if @user.valid_password?(params[:user][:password])
+              token = JsonWebToken.encode(user_id: @user.id)
+              @user.auth_token = token
+              @user.save
+            else
+              render json: {message: "Wrong password. Could not authenticate!", error: "Wrong Password", status: 401}, status: 200 and return
+            end
+          else
+            render json: {message: "User does not exist with this email.", error: 'User does not exist.', status: 404 }, status: 200 and return
+          end
+        end
+        if @user
+          @property = @user.owned_properties.new(property_update_params)
+          @property.status = "Draft"
+          if params[:property][:residential_attributes].blank? == false
+            @property.residential_attributes = residential_type_attributes_permitter
+            @property.save
+          end
+          if params[:property][:commercial_attributes].blank? == false
+            @property.commercial_attributes = commercial_type_attributes_permitter
+            @property.save
+          end
+          if params[:property][:land_attributes].blank? == false
+            @property.land_attributes = land_type_attributes_permitter
+            @property.save
+          end
+          @property.estimated_rehab_cost_attr = estimated_rehab_cost_attributes_permitter
+          @property.buy_option = buy_option_permitter
+
+          if params[:images].blank? == false
+            @property.photos.destroy_all
+            params[:images].each do |image|
+              @property.photos.create(image: image)
+            end
+          end
+          if params[:video].blank? == false
+            @property.videos.destroy_all
+            @property.videos.create(video: params[:video])
+          end
+          if params[:arv_proof].blank? == false
+            @property.arv_proofs.destroy_all
+            @property.arv_proofs.create(file: params[:property][:arv_proof], name: "Arv Proof")
+          end
+          if params[:rehab_cost_proof].blank? == false
+            @property.rehab_cost_proofs.destroy_all
+            @property.rehab_cost_proofs.create(file: params[:property][:arv_proof], name: "Rehab Cost Proof")
+          end
+          if params[:rental_proof].blank? == false
+            @property.rental_proofs.destroy_all
+            @property.rental_proofs.create(file: params[:property][:arv_proof], name: "Rental Proof")
+          end
+          if open_house_dates_permitter.blank? == false
+            @property.open_house_dates = open_house_dates_permitter
+          end
+          @property.save
+          if @property.deal_analysis_type == "Rehab & Flip Deal"
+            @property.profit_potential = params[:property][:profit_potential]
+          elsif @property.deal_analysis_type == "Landlord Deal"
+            if @property.landlord_deal
+              @landlord_deal = @property.landlord_deal
+            else
+              @landlord_deal = @property.build_landlord_deal
+              @landlord_deal.save
+            end
+            @landlord_deal.update(landlord_deal_params)
+            @landlord_deal.save
+          end
+          if @property.save
+            render json: {property: PropertySerializer.new(@property), message: "Property added sucessfully.", status: 201}, status: 200
+          else
+            render json: {message: "Property could not be added.", status: 400}, status: 200
+          end
+        end
       end
       private
       def property_params
@@ -133,27 +225,31 @@ module Api
       end
 
       def property_update_params
-        params.require(:property).permit(:address, :city, :state, :zip_code, :category, :p_type, :headliner, :mls_available, :flooded, :flood_count, :description, :seller_price, :buy_now_price, :auction_started_at, :auction_length, :auction_ending_at, :show_instructions_type_id, :seller_pay_type_id, :title_status, :youtube_url, :deal_analysis_type, :after_rehab_value, :asking_price, :estimated_rehab_cost, :profit_potential, :arv_analysis, :description_of_repairs)
+        params.require(:property).permit(:address, :city, :state, :zip_code, :category, :p_type, :headliner, :mls_available, :flooded, :flood_count, :description, :owner_category, :additional_information, :seller_price, :buy_now_price, :auction_started_at, :auction_length, :auction_ending_at, :show_instructions_type_id, :seller_pay_type_id, :title_status, :youtube_url, :deal_analysis_type, :after_rehab_value, :asking_price, :estimated_rehab_cost, :profit_potential, :arv_analysis, :description_of_repairs, :vimeo_url, :dropbox_url, :rental_description, :best_offer, :best_offer_length, :best_offer_sellers_minimum_price, :best_offer_sellers_reserve_price, :show_instructions_text)
+      end
+
+      def open_house_dates_permitter
+        JSON.parse(params[:property][:open_house_dates])
       end
 
       def buy_option_permitter
-        JSON.parse(params[:property][:buy_option].to_json)
+        JSON.parse(params[:property][:buy_option])
       end
 
       def residential_type_attributes_permitter
-        JSON.parse(params[:property][:residential_attributes].to_json)
+        JSON.parse(params[:property][:residential_attributes])
       end
       def commercial_type_attributes_permitter
-        JSON.parse(params[:property][:commercial_attributes].to_json)
+        JSON.parse(params[:property][:commercial_attributes])
       end
       def land_type_attributes_permitter
-        JSON.parse(params[:property][:land_attributes].to_json)
+        JSON.parse(params[:property][:land_attributes])
       end
-      def estimated_rehab_cost_attributes_permitter2
-        JSON.parse(params[:property][:estimated_rehab_cost_attr])
-      end
+      # def estimated_rehab_cost_attributes_permitter2
+      #   JSON.parse(params[:property][:estimated_rehab_cost_attr])
+      # end
       def estimated_rehab_cost_attributes_permitter
-        JSON.parse(params[:property][:estimated_rehab_cost_attr].to_json)
+        JSON.parse(params[:property][:estimated_rehab_cost_attr])
       end
       def landlord_deal_params
         params.require(:property).permit(:closing_cost, :short_term_financing_cost, :total_acquisition_cost, :taxes_annually, :insurance_annually, :amount_financed_percentage, :amount_financed, :interest_rate, :loan_terms, :principal_interest, :taxes_monthly, :insurance_monthly, :piti_monthly_debt, :monthly_rent, :total_gross_yearly_income, :vacancy_rate, :adjusted_gross_yearly_income, :est_annual_management_fees, :est_annual_operating_fees, :annual_debt, :net_operating_income, :annual_cash_flow, :monthly_cash_flow, :total_out_of_pocket, :roi_cash_percentage, :est_annual_operating_fees_others)
