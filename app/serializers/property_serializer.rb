14d2adcf700eb7710ea0321565df185a04acf691
rajan-gith <rajan.prajapati@berylsystems.com>
2019-12-19 19:00:58 +0530

    minor serializer changes

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -59,6 +59,7 @@ class PropertySerializer < ActiveModel::Serializer
     data[:residential_attributes] = object.category == "Residential" ? object.residential_attributes : {}
     data[:commercial_attributes] = object.category == "Commercial" ? object.commercial_attributes : {}
     data[:show_instructions] = object.show_instructions_type ? object.show_instructions_type.description : ""
+    data[:images_details] = ActiveModelSerializers::SerializableResource.new(object.photos, each_serializer: PhotoSerializer) 
     data
   end
   def property_images
