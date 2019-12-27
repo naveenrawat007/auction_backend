9989bc1d97760a9e7b012659c3d78bfadb420db0
rajan-gith <rajan.prajapati@berylsystems.com>
2019-12-27 19:07:29 +0530

    admin properties controller changes index actions improved

--- app/controllers/api/v1/admin/properties_controller.rb
+++ app/controllers/api/v1/admin/properties_controller.rb
@@ -4,22 +4,13 @@ module Api
       class PropertiesController < Api::V1::MainController
         before_action :authorize_admin_request
 
-        def under_review_properties
+        def index
           if params[:search_str].blank? == false
-            @properties = Property.where(status: "Under Review").where("lower(address) LIKE :search", search: "%#{params[:search_str].downcase}%").order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+            @properties = Property.where(status: params[:status]).where("lower(address) LIKE :search", search: "%#{params[:search_str].downcase}%").order(created_at: :desc).paginate(page: params[:page], per_page: 10)
           else
-            @properties = Property.where(status: "Under Review").order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+            @properties = Property.where(status: params[:status]).order(created_at: :desc).paginate(page: params[:page], per_page: 10)
           end
           render json: {properties: ActiveModelSerializers::SerializableResource.new(@properties, each_serializer: UnderReviewPropertySerializer), property_statuses: Property.status ,status: 200, meta: {current_page: @properties.current_page, total_pages: @properties.total_pages} }
-        end
-
-        def best_offers_properties
-          if params[:search_str].blank? == false
-            @properties = Property.where(status: "Approve / Best Offer").where("lower(address) LIKE :search", search: "%#{params[:search_str].downcase}%").order(created_at: :desc).paginate(page: params[:page], per_page: 10)
-          else
-            @properties = Property.where(status: "Approve / Best Offer").order(created_at: :desc).paginate(page: params[:page], per_page: 10)
-          end
-          render json: {properties: ActiveModelSerializers::SerializableResource.new(@properties, each_serializer: UnderReviewPropertySerializer), property_statuses: Property.status , status: 200, meta: {current_page: @properties.current_page, total_pages: @properties.total_pages} }
         end
 
         def update_status
