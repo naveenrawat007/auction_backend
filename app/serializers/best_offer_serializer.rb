8f6c5befa24119eb8ae39a22e6aafe88a5f61c38
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-02 14:41:32 +0530

    best offer serializer added

---
+++ app/serializers/best_offer_serializer.rb
@@ -0,0 +1,13 @@
+class BestOfferSerializer < ActiveModel::Serializer
+  def attributes(*args)
+    data = super
+    data[:id] = object.id
+    data[:amount] = object.amount
+    data[:type] = "Best Offer"
+    data[:user] = object.user ? object.user.first_name : ""
+    data[:user_name] = object.user ? "#{object.user.first_name} #{object.user.last_name}" : ""
+    data[:time] = object.updated_at.strftime("%m/%d/%Y | %l:%M%p")
+    data[:fund_proof] = object.get_fund_proof
+    data
+  end
+end
