27800591bbe5d1212ca2e1dac4c75bba6832c134
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-03 19:23:35 +0530

    property post auction worker added

---
+++ app/workers/property_post_auction_worker.rb
@@ -0,0 +1,16 @@
+class PropertyPostAuctionWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: 4
+
+  def perform(property_id)
+    property = Property.find_by(id: property_id)
+    if property
+      if property.status = "Approve"
+        property.status = "Post Auction"
+        if property.save
+          PropertyMailer.post_action(property.owner_id, property.id).deliver
+        end
+      end
+    end
+  end
+end
