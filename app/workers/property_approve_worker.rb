eb61947d75295935cabce5ec0b7019635c217d9b
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-03 16:37:14 +0530

    Property Aprove worker changes

--- app/workers/property_approve_worker.rb
+++ app/workers/property_approve_worker.rb
@@ -6,11 +6,18 @@ class PropertyApproveWorker
     property = Property.find_by(id: property_id)
     if property
       if property.status == "Under Review"
-        property.status = "Approve / Best Offer"
+        property.status = "Approve"
         if property.save
           PropertyMailer.approved(property.owner_id, property.id).deliver
+          if property.auction_started_at.blank? == false
+            if property.best_offer == true
+              Sidekiq::Client.enqueue_to_in("default", property.auction_started_at , PropertyBestOfferWorker, property.id)
+              Sidekiq::Client.enqueue_to_in("default", property.auction_started_at + property.auction_started_at + property.best_offer_length.to_i.days , PropertyLiveWorker, property.id)
+            else
+              Sidekiq::Client.enqueue_to_in("default", property.auction_started_at , PropertyLiveWorker, property.id)
+            end
+          end
         end
-        Sidekiq::Client.enqueue_to_in("default", property.auction_started_at , PropertyLiveWorker, property.id)
       end
     end
   end
