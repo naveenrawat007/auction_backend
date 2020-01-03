b934cf5bec93039961fa8ce733666ba0dfdd6195
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-03 16:39:54 +0530

    Property live worker changes

--- app/workers/property_live_worker.rb
+++ app/workers/property_live_worker.rb
@@ -5,7 +5,7 @@ class PropertyLiveWorker
   def perform(property_id)
     property = Property.find_by(id: property_id)
     if property
-      if property.status == "Approve / Best Offer"
+      if (property.status == "Approve" || property.status == "Best Offer" )
         property.status = "Live Online Bidding"
         property.save
         PropertyMailer.live_bidding(property.owner_id, property.id).deliver
