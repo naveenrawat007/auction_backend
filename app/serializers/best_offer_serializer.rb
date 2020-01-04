521be42f91a5cf914b5fcd1c3375ddd8b2686730
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-04 16:56:15 +0530

    best offer and bid serializer changes user type key added

--- app/serializers/best_offer_serializer.rb
+++ app/serializers/best_offer_serializer.rb
@@ -8,6 +8,23 @@ class BestOfferSerializer < ActiveModel::Serializer
     data[:user_name] = object.user ? "#{object.user.first_name} #{object.user.last_name}" : ""
     data[:time] = object.updated_at.strftime("%m/%d/%Y | %l:%M%p")
     data[:fund_proof] = object.get_fund_proof
+    data[:user_type] = user_type
     data
+  end
+  def user_type
+    u_type = ""
+    if object.user.type_attributes.blank? == false
+      object.user.type_attributes.each do |type|
+        if type[1] == true
+          u_type = type[0]
+          break
+        end
+      end
+    end
+    if u_type.include?("Inves")
+      "Investor"
+    else
+      "Realtor"
+    end
   end
 end
