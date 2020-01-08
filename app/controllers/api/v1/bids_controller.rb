0f34facec92165f651847a78e8a0fd2c78e61830
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-08 18:36:12 +0530

    property bid controller bidding sniper detection and preventation method added

--- app/controllers/api/v1/bids_controller.rb
+++ app/controllers/api/v1/bids_controller.rb
@@ -5,16 +5,26 @@ module Api
       def create
         @property = Property.find_by(id: params[:property][:id])
         if @property
-          @bid = @property.bids.where(user_id: @current_user.id).first_or_create
-          @bid.user_id = @current_user.id
-          @bid.amount = params[:bid][:amount]
-          @bid.save
-          Sidekiq::Client.enqueue_to_in("default", Time.now , PropertyBidNotificationWorker, @property.id)
-          if params[:bid][:fund_proof].blank? == false
-            @bid.fund_proofs.destroy_all
-            @bid.fund_proofs.create(file: params[:bid][:fund_proof])
+          if @property.bidding_ending_at > Time.now
+            @bid = @property.bids.where(user_id: @current_user.id).first_or_create
+            @bid.user_id = @current_user.id
+            @bid.amount = params[:bid][:amount]
+            @bid.save
+            if (@property.bidding_ending_at - Time.now < 1.minutes) #time check for bidding time
+              @property.sniper = true
+              @property.sniper_length += 3 #count of time duration increasing in minutes
+              @property.save
+              Sidekiq::Client.enqueue_to_in("default", @property.bidding_ending_at, PropertyPostAuctionWorker, @property.id)
+            end
+            Sidekiq::Client.enqueue_to_in("default", Time.now , PropertyBidNotificationWorker, @property.id)
+            if params[:bid][:fund_proof].blank? == false
+              @bid.fund_proofs.destroy_all
+              @bid.fund_proofs.create(file: params[:bid][:fund_proof])
+            end
+            render json: {property: PropertySerializer.new(@property), message: "Bid Created.", status: 201 }, status: 200
+          else
+            render json: {message: "Auction time period is over.", status: "404" }, status: 200
           end
-          render json: {property: PropertySerializer.new(@property), message: "Bid Created.", status: 201 }, status: 200
         else
           render json: {message: "Property Not Found.", status: "404"}, status: 200
         end
