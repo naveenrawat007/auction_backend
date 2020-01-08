99dffcbd0c8dd7d8825f801c1d141f51ce861b41
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-08 12:51:18 +0530

    user model changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   has_one :photo, as: :imageable
   has_many :owned_properties, class_name: "Property", foreign_key: "owner_id"
   has_many :bids
+  has_many :bidded_properties, through: :bids, source: :property
   has_many :best_offers
+  has_many :best_offered_properties, through: :best_offers, source: :property
   has_many :buy_nows, -> { where(best_offer: false) }, class_name: "BuyNowOffer"
   has_many :best_buy_nows, -> { where(best_offer: true) }, class_name: "BuyNowOffer"
+  has_many :buy_now_offers
+  has_many :buy_now_offered_properties, through: :buy_now_offers, source: :property
 
   has_many :user_watch_properties
   has_many :watch_properties, through: :user_watch_properties, source: :property
