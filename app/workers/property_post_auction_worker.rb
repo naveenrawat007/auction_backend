80bcc5abbd902cc903c85044a3c4822ae09c278b
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-13 18:43:03 +0530

    poperty post auction compared

--- app/workers/property_post_auction_worker.rb
+++ app/workers/property_post_auction_worker.rb
@@ -6,13 +6,15 @@ class PropertyPostAuctionWorker
     property = Property.find_by(id: property_id)
     if property
       if (property.status == "Approve" || property.status == "Live Online Bidding" || property.status == "Best Offer" )
-        if property.sniper == true
-          property.sniper = false
-          property.save
-        else
-          property.status = "Post Auction"
-          if property.save
-            PropertyMailer.post_auction(property.owner_id, property.id).deliver
+        if property.post_auction_worker_jid == self.jid
+          if property.sniper == true
+            property.sniper = false
+            property.save
+          else
+            property.status = "Post Auction"
+            if property.save
+              PropertyMailer.post_auction(property.owner_id, property.id).deliver
+            end
           end
         end
       end
