7f8495d66c809f4e94363324aae13e8ad817a7fc
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-13 12:47:01 +0530

    token returned with out serialization option

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   def attributes(*args)
     data = super
-    if self.instance_options[:serializer_options]
-      data[:token] = self.instance_options[:serializer_options][:token]
-    end
+    data[:token] = object.auth_token
     data[:user_image] = user_image
     data[:id] = object.id
     data[:first_name] = object.first_name ? object.first_name : ""
