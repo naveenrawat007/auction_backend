5cc6a658a8b96c7662023f2371b3b9b23dc9c0a1
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-17 12:10:43 +0530

    cabel channel file renamed

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,8 +1,8 @@
-class GroupChannel < ApplicationCable::Channel
+class RoomChannel < ApplicationCable::Channel
   # calls when a client connects to the server
   def subscribed
-    if params[:group_id].present?
-      stream_from("ChatRoom-#{(params[:group_id])}")
+    if params[:room_id].present?
+      stream_from("ChatRoom-#{(params[:room_id])}")
     end
   end
 
@@ -10,29 +10,29 @@ class GroupChannel < ApplicationCable::Channel
   def speak(data)
     # puts"11111111111111111111111111111111111111111111111111111111111111"
     sender    = get_sender(data['sender'])
-    group_id   = data['group_id']
+    room_id   = data['room_id']
     message   = data['message']
-
-    raise 'No room_id!' if group_id.blank?
-    group = get_group(group_id) # A conversation is a room
-    raise 'No conversation found!' if group.blank?
+    puts "#{room_id}"
+    raise 'No room_id!' if room_id.blank?
+    chat_room = get_chat_room(room_id)
+    raise 'No Chat Room found!' if chat_room.blank?
     raise 'No message!' if message.blank?
 
     # adds the message sender to the conversation if not already included
-    group.users << sender unless group.users.include?(sender)
+    chat_room.users << sender unless chat_room.users.include?(sender)
     # saves the message and its data to the DB
     # Note: this does not broadcast to the clients yet!
     message = Message.create(
-      group: group,
+      chat_room: chat_room,
       user: sender,
       content: message
     )
     payload = {
-      group_id: message.group.id,
+      chat_room_id: message.chat_room_id,
       content: message.content,
       sender: message.user_id,
     }
-    ActionCable.server.broadcast(build_room_id(message.group.id), payload)
+    ActionCable.server.broadcast(build_room_id(message.chat_room_id), payload)
 
   end
 
@@ -41,8 +41,8 @@ class GroupChannel < ApplicationCable::Channel
     "ChatRoom-#{id}"
   end
 
-  def get_group(group_id)
-    Group.find_by(id: group_id)
+  def get_chat_room(room_id)
+    ChatRoom.find_by(id: room_id)
   end
 
   def get_sender(id)
