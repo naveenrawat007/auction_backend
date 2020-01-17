a6e7418a67c125b646d338ceb8beea28287afe6e
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-17 14:11:44 +0530

    chat room serializer owner and property details added

--- app/serializers/chat_room_serializer.rb
+++ app/serializers/chat_room_serializer.rb
@@ -3,6 +3,9 @@ class ChatRoomSerializer < ActiveModel::Serializer
     data = super
     data[:id] = object.id
     data[:name] = object.name
+    data[:owner_name] = object.property.owner.full_name
+    data[:owner_img] = object.property.owner.user_image
+    data[:property_name] = object.property.address
     data
   end
 end
