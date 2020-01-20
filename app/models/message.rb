b806238df276be3bbbbc6b7470229495e1271791
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-20 19:24:21 +0530

    message model association to attachments added

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
   after_create_commit { MessageBroadcastWorker.perform_at(Time.now, self.id) }
   belongs_to :chat_room
   belongs_to :user
+  has_many :attachments, as: :resource, dependent: :destroy 
 end
