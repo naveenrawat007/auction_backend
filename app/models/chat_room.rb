93d41093219d1f2f3e3255e33eaff558a4439155
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-21 17:47:09 +0530

    associations updated

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -3,4 +3,5 @@ class ChatRoom < ApplicationRecord
   has_many :users, through: :user_chat_rooms
   has_many :messages, dependent: :destroy
   belongs_to :property
+  belongs_to :offer, polymorphic: true
 end
