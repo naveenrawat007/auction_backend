0b0dc92dea2d8455d81237a1ba6e52b8be629568
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-28 18:12:20 +0530

    model side updation on buy offers

--- app/models/buy_now_offer.rb
+++ app/models/buy_now_offer.rb
@@ -2,7 +2,7 @@ class BuyNowOffer < ApplicationRecord
   belongs_to :property
   belongs_to :user
   has_many :fund_proofs, as: :resource, class_name: "Attachment", dependent: :destroy
-  has_one :chat_room, as: :offer
+  has_one :chat_room, as: :offer, dependent: :destroy
 
   def get_fund_proof
     if self.fund_proofs.blank? == false
