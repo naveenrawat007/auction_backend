d2951c48336e6971eec85666c09e457c5243d269
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-28 17:35:17 +0530

    Chat room serializer users list response added

--- app/serializers/chat_room_serializer.rb
+++ app/serializers/chat_room_serializer.rb
@@ -5,6 +5,7 @@ class ChatRoomSerializer < ActiveModel::Serializer
     data[:name] = object.name
     data[:owner_name] = object.property.owner.full_name
     data[:owner_image] = object.property.owner.user_image
+    data[:users] = ActiveModel::Serializer::CollectionSerializer.new(object.users.where(is_admin: false), serializer: UserChatSerializer)
     data[:property_name] = object.property.address
     data
   end
