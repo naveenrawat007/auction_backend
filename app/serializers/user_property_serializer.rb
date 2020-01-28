9be722f415a511fc9b238cb1b8c275a670321407
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-28 16:05:57 +0530

    user property serializer added for listing properties

---
+++ app/serializers/user_property_serializer.rb
@@ -0,0 +1,75 @@
+class UserPropertySerializer < ActiveModel::Serializer
+  def attributes(*args)
+    data = super
+    data[:created_at] = object.created_at.strftime("%m/%d/%Y")
+    data[:id] = object.id
+    data[:address] = object.address ? object.address : ""
+    data[:headliner] = object.headliner ? object.headliner : ""
+    data[:best_offer_sellers_minimum_price] = object.best_offer_sellers_minimum_price ? object.best_offer_sellers_minimum_price : ""
+    data[:best_offer_price] = object.best_offer_price
+    data[:best_offer_sellers_reserve_price] = object.best_offer_sellers_reserve_price ? object.best_offer_sellers_reserve_price : ""
+    data[:vimeo_url] = object.vimeo_url ? object.vimeo_url : ""
+    data[:dropbox_url] = object.dropbox_url ? object.dropbox_url : ""
+    data[:asking_price] = object.asking_price ? object.asking_price : ""
+    data[:seller_price] = object.seller_price ? object.seller_price : ""
+    data[:buy_now_price] = object.buy_now_price ? object.buy_now_price : ""
+    data[:status] = object.status
+    data[:total_views] = object.total_views ? object.total_views : ""
+    data[:arv_proof] = arv_proof
+    data[:rehab_cost_proof] = rehab_cost_proof
+    data[:rental_proof] = rental_proof
+    data[:lat] = object.lat ? object.lat : ""
+    data[:long] = object.long ? object.long : ""
+    data[:highest_bid] = object.highest_bid
+    data[:unique_address] = object.unique_address
+    data[:bids] = ActiveModelSerializers::SerializableResource.new(object.bids, each_serializer: BidSerializer)
+    data[:best_offers] = ActiveModelSerializers::SerializableResource.new(object.best_offers, each_serializer: BestOfferSerializer)
+    data[:buy_now_offers] = ActiveModelSerializers::SerializableResource.new(object.buy_now_offers, each_serializer: BuyNowSerializer)
+    data[:thumbnail_img] = get_thumbnail
+    if self.instance_options[:serializer_options]
+      if self.instance_options[:serializer_options][:user] && self.instance_options[:serializer_options][:type]
+        if self.instance_options[:serializer_options][:type] == "offer"
+          data[:chat_room] = self.instance_options[:serializer_options][:user].best_offers.find_by(property_id: object.id) ?  ChatRoomSerializer.new(self.instance_options[:serializer_options][:user].best_offers.find_by(property_id: object.id).chat_room) : ""
+        elsif self.instance_options[:serializer_options][:type] == "bid"
+          data[:chat_room] = self.instance_options[:serializer_options][:user].bids.find_by(property_id: object.id) ? ChatRoomSerializer.new(self.instance_options[:serializer_options][:user].bids.find_by(property_id: object.id).chat_room) : ""
+        elsif self.instance_options[:serializer_options][:type] == "buy_now"
+          data[:chat_room] = self.instance_options[:serializer_options][:user].buy_now_offers.find_by(property_id: object.id) ? ChatRoomSerializer.new(self.instance_options[:serializer_options][:user].buy_now_offers.find_by(property_id: object.id).chat_room) : ""
+        end
+      end
+    end
+    data
+  end
+
+  def arv_proof
+    if object.arv_proofs.last
+      APP_CONFIG['backend_site_url'] + object.arv_proofs.last.file.url
+    else
+      ""
+    end
+  end
+
+  def rehab_cost_proof
+    if object.rehab_cost_proofs.last
+      APP_CONFIG['backend_site_url'] + object.rehab_cost_proofs.last.file.url
+    else
+      ""
+    end
+  end
+
+  def rental_proof
+    if object.rental_proofs.last
+      APP_CONFIG['backend_site_url'] + object.rental_proofs.last.file.url
+    else
+      ""
+    end
+  end
+
+  def get_thumbnail
+    if object.photos.blank? == false
+      APP_CONFIG['backend_site_url'] + object.photos.first.image.url(:thumb)
+    else
+      ""
+    end
+  end
+
+end
