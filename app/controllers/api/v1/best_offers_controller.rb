2940ebe33bd752c91dfaa098b6844a46e5a9c5d8
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-28 18:10:49 +0530

    best offer controller msg created

--- app/controllers/api/v1/best_offers_controller.rb
+++ app/controllers/api/v1/best_offers_controller.rb
@@ -29,6 +29,13 @@ module Api
                 @chat_room.save
                 @chat_room.users << @property.owner
                 @chat_room.users << @current_user
+                message = @chat_room.messages.new
+                message.content = "I have submitted Best Offer in your property at #{@property.address} for $#{@best_offer.amount} and check proof of funds."
+                message.user_id = @current_user.id
+                message.save
+                if @best_offer.fund_proofs.blank? == false
+                  message.attachments.create(file: File.open(@best_offer.fund_proofs.first.file.path,'rb'))
+                end
               end
               #end
               render json: {chat_room: ChatRoomSerializer.new(@chat_room), property: PropertySerializer.new(@property), message: "Best Offer Submitted.", status: 201 }, status: 200
