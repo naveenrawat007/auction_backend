b02065bc1618e5ca8cbffcf0de8dd94a3dd2be2a
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-29 15:16:43 +0530

    wwelcome mailer action added

--- app/mailers/user_verification_mailer.rb
+++ app/mailers/user_verification_mailer.rb
@@ -3,4 +3,9 @@ class UserVerificationMailer < ApplicationMailer
 	  @user = user
     mail(to: [@user.email], subject: "Verify your email (Auction my deal).")
 	end
+  def welcome(user)
+    @user = user
+    mail(to: [@user.email], subject: "Welcome to AuctionMyDeal.com… 😊")
+    UserMessage.welcome(@user)
+	end
 end
