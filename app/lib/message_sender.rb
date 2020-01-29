0af92e472a2d2e4d3bb3e5bb9f6d2a237a6f2554
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-29 15:22:00 +0530

    message sender class send action added

--- app/lib/message_sender.rb
+++ app/lib/message_sender.rb
@@ -10,6 +10,17 @@ class MessageSender
       to:    phone_number,
       body:  code
     )
-    # message.status == 'queued'
+  end
+  def self.send(phone_number, message)
+    # puts "yess++++++++++++++++++++++++++++++++++++++++++#{phone_number}++++#{message}"
+    account_sid =  Rails.application.credentials.twilio_account_sid
+    auth_token  =  Rails.application.credentials.twilio_auth_token
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    message = client.messages.create(
+      from:  Rails.application.credentials.twilio_phone_number,
+      to:    phone_number,
+      body:  message
+    )
   end
 end
