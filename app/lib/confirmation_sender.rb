4186f55851182acec36f50c3bacf9717c563c5c0
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-29 15:19:22 +0530

    verification message template changed in confirmation sender

--- app/lib/confirmation_sender.rb
+++ app/lib/confirmation_sender.rb
@@ -4,17 +4,8 @@ class ConfirmationSender
     user.update(verification_code: code)
     begin
       Sidekiq::Client.enqueue_to_in("default", Time.now, UserVerificationWorker, user.id)
-      # c = Geocoder.search(ip_address).first.country unless ip_address.blank?
-      # if c
-      #   c_details = ISO3166::Country.new(c)
-      #   @country_code = c_details.data["country_code"]
-      # else
-      #   @country_code = "91"
-      # end
-      #MessageSender.send_code("+91#{user.phone_number}", verification_code)
-      @country_code = "1"
-      verification_code = "Welcome to Auction my deal. Here is your verification code: #{user.verification_code} "
-      MessageSender.send_code("+#{@country_code.to_i}#{user.phone_number}", verification_code)
+      verification_code = "Please enter the following verification code to activate your account at www.AuctionMyDeal.com.  Verification Code: #{user.verification_code} "
+      MessageSender.send_code("+1#{user.phone_number}", verification_code)
     rescue StandardError => e
     end
   end
