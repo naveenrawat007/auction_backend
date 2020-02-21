4aad091ef2b3c60e53b30e9c890cc305bbf26567
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-21 17:35:43 +0530

    best offer post auction worker added

---
+++ app/workers/property_best_offer_post_auction_worker.rb
@@ -0,0 +1,17 @@
+class PropertyBestOfferPostAuctionWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: 4
+
+  def perform(property_id)
+    property = Property.find_by(id: property_id)
+    if property
+      if property.best_offer_post_auction_worker_jid == self.jid || property.best_offer_post_auction_worker_jid.blank? == true
+        if property.status == "Best Offer"
+          property.status = "Hold"
+          property.save
+          PropertyMailer.best_offer_ended(property.owner_id, property.id).deliver
+        end
+      end
+    end
+  end
+end
