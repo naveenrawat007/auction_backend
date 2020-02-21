94dbb084b65e0d0e80d4778df435e27928a5eeb3
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-21 17:40:47 +0530

    helper methods added for population workers job ids

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def best_offer_live_auction(property)
+    @property = property
+    @property.best_offer_live_auction_worker_jid = Sidekiq::Client.enqueue_to_in("default", @property.best_offer_auction_started_at , PropertyBestOfferWorker, @property.id)
+    @property.save
+  end
+
+  def best_offer_post_auction(property)
+    @property = property
+    @property.best_offer_post_auction_worker_jid = Sidekiq::Client.enqueue_to_in("default", @property.best_offer_auction_ending_at, PropertyBestOfferPostAuctionWorker, @property.id)
+    @property.save
+  end
+
+  def live_auction(property)
+    @property = property
+    @property.live_auction_worker_jid = Sidekiq::Client.enqueue_to_in("default", @property.auction_started_at , PropertyLiveWorker, @property.id)
+    @property.save
+  end
+
+  def post_auction(property)
+    @property = property
+    @property.post_auction_worker_jid = Sidekiq::Client.enqueue_to_in("default", @property.auction_bidding_ending_at, PropertyPostAuctionWorker, @property.id)
+    @property.save
+  end
 end
