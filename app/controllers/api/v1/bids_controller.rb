111eac177f57f609bd4bf4898a73c7f34820bb20
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-21 17:39:05 +0530

    property bidding timing changes in bids controller

--- app/controllers/api/v1/bids_controller.rb
+++ app/controllers/api/v1/bids_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class BidsController < MainController
       before_action :authorize_request
+      include ApplicationHelper
       def create
         @property = Property.find_by(id: params[:property][:id])
         if @property
@@ -9,18 +10,18 @@ module Api
             render json: {property: PropertySerializer.new(@property), message: "Can not submit proposal on own property.", status: 400 }, status: 200 and return
           else
             hightest_bid_offer = @property.highest_bid
-            if @property.bidding_ending_at > Time.now
+            if @property.auction_bidding_ending_at > Time.now
               if params[:bid][:amount].to_f > hightest_bid_offer.to_f
                 @bid = @property.bids.where(user_id: @current_user.id).first_or_create
                 @bid.user_id = @current_user.id
                 @bid.amount = params[:bid][:amount]
                 @bid.buy_option = buy_option_permitter
                 @bid.save
-                if (@property.bidding_ending_at - Time.now < 1.minutes) #time check for bidding time
+                if (@property.auction_bidding_ending_at - Time.now < 1.minutes) #time check for bidding time
                   @property.sniper = true
                   @property.sniper_length += 3 #count of time duration increasing in minutes
                   @property.save
-                  post_auction_worker_jid = Sidekiq::Client.enqueue_to_in("default", @property.bidding_ending_at, PropertyPostAuctionWorker, @property.id)
+                  post_auction(@property)
                   @property.post_auction_worker_jid = post_auction_worker_jid
                   @property.save
                 end
