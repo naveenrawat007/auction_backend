f7ee625a3f7b0dcde640835ac2008b7a8b9b48bb
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-22 16:31:22 +0530

    property messages bug fix

--- app/messages/property_message.rb
+++ app/messages/property_message.rb
@@ -1,5 +1,5 @@
 class PropertyMessage
-  def under_review(user, property)
+  def self.under_review(user, property)
     begin
       message = "Thank you for submitting your property at #{property.address}! Our staff will evaluate your property to determine if it meets these requirements, and will get back to you in less than 12 to 24 hours."
       MessageSender.send("+1#{user.phone_number}", message)
