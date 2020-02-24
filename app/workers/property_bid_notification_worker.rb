bc41b7a124ab188b8a9528d64a2c1250eccbc387
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-24 18:11:27 +0530

    buyer is notified on submitting buy now offer

--- app/workers/property_bid_notification_worker.rb
+++ app/workers/property_bid_notification_worker.rb
@@ -2,10 +2,14 @@ class PropertyBidNotificationWorker
   include Sidekiq::Worker
   sidekiq_options retry: 4
 
-  def perform(property_id)
+  def perform(property_id, bid_id)
     property = Property.find_by(id: property_id)
+    bid = Bid.find_by(id: bid_id)
     if property
       PropertyMailer.bid_notification(property.owner_id, property.id).deliver
+      if bid
+        BidMailer.submitted(bid.id).deliver
+      end
     end
   end
 end
