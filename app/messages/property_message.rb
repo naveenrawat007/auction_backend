2df571b14dda29d4c1432aa867bb5ddbe50d08b3
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-24 12:42:25 +0530

    proeprty msg for under review/ submitted changes

--- app/messages/property_message.rb
+++ app/messages/property_message.rb
@@ -1,7 +1,7 @@
 class PropertyMessage
   def self.under_review(user, property)
     begin
-      message = "Thank you for submitting your property at #{property.address}! Our staff will evaluate your property to determine if it meets these requirements, and will get back to you in less than 12 to 24 hours."
+      message = "Thank you for submitting your property at #{property.address}!  You will get an email and a message in your user dashboard that will advise you when your property is approved or if we have any questions. To Your Success, \n Your Support Team @ \n AuctionMyDeal.com "
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
