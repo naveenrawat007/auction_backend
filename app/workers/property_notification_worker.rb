918132b9a48b91fdc8ebb77c12f555139bc9dd7c
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-24 15:52:47 +0530

    notification worker updated

--- app/workers/property_notification_worker.rb
+++ app/workers/property_notification_worker.rb
@@ -1,11 +1,16 @@
 class PropertyNotificationWorker
   include Sidekiq::Worker
   sidekiq_options retry: 4
-
   def perform(property_id)
     property = Property.find_by(id: property_id)
     if property
-      PropertyMailer.status_notification(property.owner_id, property.id).deliver
+      if property.status == "Hold"
+        PropertyMailer.hold_notification(property.owner_id, property.id).deliver
+      elsif property.status == "Terminated"
+        PropertyMailer.termination_notification(property.owner_id, property.id).deliver
+      else
+        PropertyMailer.status_notification(property.owner_id, property.id).deliver
+      end
     end
   end
 end
