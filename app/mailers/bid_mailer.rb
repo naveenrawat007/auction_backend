deca57e570969112fb0904b14f9d5c9f042ffcb5
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-24 18:10:34 +0530

    bid mailer bug fix

--- app/mailers/bid_mailer.rb
+++ app/mailers/bid_mailer.rb
@@ -1,6 +1,6 @@
 class BidMailer < ApplicationMailer
   def out_bidded(bid_id)
-    @offer = Bid.find_by(id: offer_id)
+    @offer = Bid.find_by(id: bid_id)
     if @offer
   	  @buyer = User.find_by(id: @offer.user_id)
       @property = Property.find_by(id: @offer.property_id)
@@ -11,12 +11,12 @@ class BidMailer < ApplicationMailer
 	end
 
   def submitted(bid_id)
-    @offer = Bid.find_by(id: offer_id)
+    @offer = Bid.find_by(id: bid_id)
     if @offer
   	  @buyer = User.find_by(id: @offer.user_id)
       @property = Property.find_by(id: @offer.property_id)
       if @buyer && @property
-        mail(to: [@buyer.email], subject: "#{@property.address} is now Pending.")
+        mail(to: [@buyer.email], subject: " Thank you for Your Offer on #{@property.address}!")
       end
     end
   end
