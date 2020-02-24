fe6b09ac0cb31e252e67dad98bf9ab79866bfbcb
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-24 12:44:00 +0530

    forget password msg changes

--- app/messages/user_message.rb
+++ app/messages/user_message.rb
@@ -8,7 +8,7 @@ class UserMessage
   end
   def self.forget_password(user)
     begin
-      message = "We heard you need a password reset. Please check your email to reset your password."
+      message = "We heard you need a password reset. Click the link below and you'll be redirected to a secure site from which you can set a new password. \n #{APP_CONFIG['site_url']}/new_password?reset_token=#{user.auth_token} \n If you didn't try to reset your password then ignore this email. \n Best Regards, \n Your Support Team @ \n AuctionMyDeal.com"
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
