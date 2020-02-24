edf6cd473061121708619628656fc08527280595
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-24 17:46:19 +0530

    bid mailer added

---
+++ app/mailers/bid_mailer.rb
@@ -0,0 +1,23 @@
+class BidMailer < ApplicationMailer
+  def out_bidded(bid_id)
+    @offer = Bid.find_by(id: offer_id)
+    if @offer
+  	  @buyer = User.find_by(id: @offer.user_id)
+      @property = Property.find_by(id: @offer.property_id)
+      if @buyer && @property
+        mail(to: [@buyer.email], subject: "#{@property.address} is now Pending.")
+      end
+    end
+	end
+
+  def submitted(bid_id)
+    @offer = Bid.find_by(id: offer_id)
+    if @offer
+  	  @buyer = User.find_by(id: @offer.user_id)
+      @property = Property.find_by(id: @offer.property_id)
+      if @buyer && @property
+        mail(to: [@buyer.email], subject: "#{@property.address} is now Pending.")
+      end
+    end
+  end
+end
