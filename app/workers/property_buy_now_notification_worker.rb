c44dcfdf5eb82b1ca1b0109750f2a2d23bfa997d
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-24 18:25:58 +0530

    property buy now notification worker updated

--- app/workers/property_buy_now_notification_worker.rb
+++ app/workers/property_buy_now_notification_worker.rb
@@ -2,10 +2,11 @@ class PropertyBuyNowNotificationWorker
   include Sidekiq::Worker
   sidekiq_options retry: 4
 
-  def perform(property_id)
+  def perform(property_id, buy_now_id)
     property = Property.find_by(id: property_id)
-    if property
+    if property && buy_now_id
       PropertyMailer.buy_now_notification(property.owner_id, property.id).deliver
+      BuyNowMailer.submitted(buy_now_id)
     end
   end
 end
