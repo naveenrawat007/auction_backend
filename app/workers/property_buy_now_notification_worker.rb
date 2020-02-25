ac10bb7902ed01eb533bff17823446cd0737b479
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-25 12:50:55 +0530

    out bid mailer called on buy now

--- app/workers/property_buy_now_notification_worker.rb
+++ app/workers/property_buy_now_notification_worker.rb
@@ -7,6 +7,9 @@ class PropertyBuyNowNotificationWorker
     if property && buy_now_id
       PropertyMailer.buy_now_notification(property.owner_id, property.id).deliver
       BuyNowMailer.submitted(buy_now_id)
+      property.bids.each do |bid_offer|
+        BuyNowMailer.out_bidded_notification(bid_offer.user_id, property.id)
+      end
     end
   end
 end
