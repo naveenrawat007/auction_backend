f3fed21257e4132fa03e14daf1a28d1754a61756
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-25 14:55:35 +0530

    welcome message updated

--- app/messages/user_message.rb
+++ app/messages/user_message.rb
@@ -1,7 +1,7 @@
 class UserMessage
   def self.welcome(user)
     begin
-      message = "Thank you for registering as a free member at www.AuctionMyDeal.com!  Your account has been verified. Enjoy your 60 day free trial."
+      message = "Thank you for registering as a free member at www.AuctionMyDeal.com!  Your account has been verified and we look forward to helping you grow your real estate investment business in 2020 and beyond."
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
