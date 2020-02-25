5034a5c6a7e29c6600339d69061bcce8dd5c6f07
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-25 14:08:25 +0530

    property termination notification woker added

---
+++ app/workers/property_termination_notification_worker.rb
@@ -0,0 +1,16 @@
+class PropertyTerminationNotificationWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: 4
+  def perform(property_id, user_ids, offer_type)
+    property = Property.find_by(id: property_id)
+    if property
+      if offer_type == "Bid"
+        if user_ids.blank? == false
+          user_ids.each do |user_id|
+            BidMailer.termination_notification(user_id, property.id)
+          end
+        end
+      end
+    end
+  end
+end
