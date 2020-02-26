e0e2859a8586f9dd1f18499cb14171033c42afb4
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-26 19:20:08 +0530

    bid mailer withdraw notification worker added and view added

---
+++ app/workers/property_withdrawn_notification_worker.rb
@@ -0,0 +1,16 @@
+class PropertyWithdrawnNotificationWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: 4
+  def perform(property_id, user_ids, offer_type)
+    property = Property.find_by(id: property_id)
+    if property
+      if offer_type == "Bid"
+        if user_ids.blank? == false
+          user_ids.each do |user_id|
+            BidMailer.withdrawn_notification(user_id, property.id).deliver
+          end
+        end
+      end
+    end
+  end
+end
