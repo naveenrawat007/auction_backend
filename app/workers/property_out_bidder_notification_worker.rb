d59eeab82bffed7953b5f039f0bc55b591ec4a1e
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-26 14:45:08 +0530

    minor change

--- app/workers/property_out_bidder_notification_worker.rb
+++ app/workers/property_out_bidder_notification_worker.rb
@@ -5,8 +5,8 @@ class PropertyOutBidderNotificationWorker
   def perform(property_id)
     property = Property.find_by(id: property_id)
     if property
-      property.bids.where.not(amount: property.bids.maximum(:amount)).each do |best_offer|
-        BidMailer.out_bidded(best_offer.id).deliver
+      property.bids.where.not(amount: property.bids.maximum(:amount)).each do |bid_offer|
+        BidMailer.out_bidded(bid_offer.id).deliver
       end
     end
   end
