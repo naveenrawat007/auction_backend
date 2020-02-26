148cb133563cba88e05ab60ac48b1d922ad63d75
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-26 12:55:20 +0530

    message notification worker  bug fix

--- app/workers/message_notification_worker.rb
+++ app/workers/message_notification_worker.rb
@@ -2,7 +2,7 @@ class MessageNotificationWorker
   include Sidekiq::Worker
   sidekiq_options retry: 4
 
-  def perform(property_id)
+  def perform(user_id)
     user = User.find_by(id: user_id)
     if user
       PropertyMessage.chat_notification(user)
