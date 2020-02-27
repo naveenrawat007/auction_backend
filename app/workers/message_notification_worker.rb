147a653e9b930ef77cf48cc9d2a4b5fbb935e178
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-27 12:57:52 +0530

    notification worker changes and worker removed from message broadcast worker

--- app/workers/message_notification_worker.rb
+++ app/workers/message_notification_worker.rb
@@ -2,10 +2,13 @@ class MessageNotificationWorker
   include Sidekiq::Worker
   sidekiq_options retry: 4
 
-  def perform(user_id)
+  def perform(user_id, message_id)
     user = User.find_by(id: user_id)
-    if user
-      PropertyMessage.chat_notification(user)
+    message = Message.find_by(id: message_id)
+    if user && message
+      if message.recipient_read == false
+        PropertyMessage.chat_notification(user)
+      end
     end
   end
 end
