b1c0467dc67acc95b1354ade6faf20bb2de31f84
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-05 21:58:54 +0530

    serializer time formatting removed

--- app/serializers/under_review_property_serializer.rb
+++ app/serializers/under_review_property_serializer.rb
@@ -13,15 +13,15 @@ class UnderReviewPropertySerializer < ActiveModel::Serializer
     data[:residential_attributes] = object.category == "Residential" ? object.residential_attributes : {}
     data[:commercial_attributes] = object.category == "Commercial" ? object.commercial_attributes : {}
     data[:status] = object.status
-    data[:submitted_at] = object.submitted_at ? object.submitted_at.strftime("%m/%d/%Y") : ""
-    data[:termination_date] = object.termination_date ? object.termination_date.strftime("%m/%d/%Y") : ""
+    data[:submitted_at] = object.submitted_at ? object.submitted_at : ""
+    data[:termination_date] = object.termination_date ? object.termination_date : ""
     data[:termination_reason] = object.termination_reason ? object.termination_reason : ""
-    data[:auction_started_at] = object.auction_started_at ? object.auction_started_at.strftime("%m/%d/%Y") : ""
+    data[:auction_started_at] = object.auction_started_at ? object.auction_started_at : ""
     data[:auction_bidding_ending_at] = object.auction_bidding_ending_at ? object.auction_bidding_ending_at : ""
     data[:auction_length] = object.auction_length
     data[:best_offer] = object.best_offer
     data[:auction_started_at_date] = object.auction_started_at ? object.auction_started_at : ""
-    data[:best_offer_auction_started_at] = object.best_offer_auction_started_at ? object.best_offer_auction_started_at.strftime("%m/%d/%Y") : ""
+    data[:best_offer_auction_started_at] = object.best_offer_auction_started_at ? object.best_offer_auction_started_at : ""
     data[:best_offer_auction_ending_at] = object.best_offer_auction_ending_at ? object.best_offer_auction_ending_at : ""
     data[:submitted_at_timer] = submitted_timer
     data[:unique_address] = object.unique_address
@@ -30,7 +30,7 @@ class UnderReviewPropertySerializer < ActiveModel::Serializer
     data[:highest_bid_detail] = object.bids.order(:amount).first ? BidSerializer.new(object.bids.order(:amount).first) : {}
     data[:best_offers] = ActiveModelSerializers::SerializableResource.new(object.best_offers, each_serializer: BestOfferSerializer)
     data[:buy_now_offers] = ActiveModelSerializers::SerializableResource.new(object.buy_now_offers, each_serializer: BuyNowSerializer)
-    data[:requested_at] = object.requested_at ? object.requested_at.strftime("%m/%d/%Y") : ""
+    data[:requested_at] = object.requested_at ? object.requested_at : ""
     data[:request_reason] = object.request_reason ? object.request_reason : ""
     data[:requested_status] = object.requested_status ? object.requested_status : ""
     data[:auction_type] = object.requested ? "Withdraw / Draft" : (object.status == "Under Review" ? "Under Review" : (object.best_offer ? "Best Offer" : "Live Online Bidding"))
