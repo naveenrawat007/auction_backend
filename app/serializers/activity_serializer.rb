2ec531de8aa063345615c5edb082c8d0a80ef059
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-06 16:44:06 +0530

    activity serializer added

---
+++ app/serializers/activity_serializer.rb
@@ -0,0 +1,11 @@
+class ActivitySerializer < ActiveModel::Serializer
+  include ActionView::Helpers::DateHelper
+  def attributes(*args)
+    data = super
+    data[:id] = object.id
+    data[:user] = object.resource_name
+    data[:desc] = object.description
+    data[:time] = time_ago_in_words(object.created_at) + " ago"
+    data
+  end
+end
