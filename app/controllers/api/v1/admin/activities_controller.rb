56783c0c4ce70cc7b6a709a1a8dc0f2a75a6e6ea
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-06 16:36:35 +0530

    activities controller added

---
+++ app/controllers/api/v1/admin/activities_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    module Admin
+      class ActivitiesController < Api::V1::MainController
+        before_action :authorize_admin_request
+        def index
+          params[:page] ||= 1
+          activites = Activity.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+          render json: {activites: ActiveModelSerializers::SerializableResource.new(activites, each_serializer: ActivitySerializer), status: 200, meta: {current_page: activites.current_page, total_pages: activites.total_pages}}, status: 200
+        end
+      end
+    end
+  end
+end
