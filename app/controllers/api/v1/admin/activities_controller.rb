628437ed64cee40aca3cc80a2fb0e3e6e10873d3
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-06 18:04:01 +0530

    controller typo fix

--- app/controllers/api/v1/admin/activities_controller.rb
+++ app/controllers/api/v1/admin/activities_controller.rb
@@ -6,7 +6,7 @@ module Api
         def index
           params[:page] ||= 1
           activites = Activity.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
-          render json: {activites: ActiveModelSerializers::SerializableResource.new(activites, each_serializer: ActivitySerializer), status: 200, meta: {current_page: activites.current_page, total_pages: activites.total_pages}}, status: 200
+          render json: {activities: ActiveModelSerializers::SerializableResource.new(activites, each_serializer: ActivitySerializer), status: 200, meta: {current_page: activites.current_page, total_pages: activites.total_pages}}, status: 200
         end
       end
     end
