a6e3cbc6f002ee4fc7657978b38c6bfa49aeb7e9
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-07 15:37:33 +0530

    activities controller changes notification fetched

--- app/controllers/api/v1/admin/activities_controller.rb
+++ app/controllers/api/v1/admin/activities_controller.rb
@@ -5,7 +5,11 @@ module Api
         before_action :authorize_admin_request
         def index
           params[:page] ||= 1
-          activites = Activity.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+          if params[:type] == "notification"
+            activites = Activity.where.not(act_type: ['user_login','user_register','user_password_change','bid_submission','offer_submission']).where(viewed: false).order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+          else
+            activites = Activity.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+          end
           render json: {activities: ActiveModelSerializers::SerializableResource.new(activites, each_serializer: ActivitySerializer), status: 200, meta: {current_page: activites.current_page, total_pages: activites.total_pages}}, status: 200
         end
       end
