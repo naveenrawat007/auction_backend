905d83f5fa58c4e2d1f628049a5e493b7548ece4
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-07 16:33:32 +0530

    activity serializer property serialized and rendered

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -6,6 +6,12 @@ class ActivitySerializer < ActiveModel::Serializer
     data[:user] = object.resource_name
     data[:description] = object.description
     data[:time] = time_ago_in_words(object.created_at) + " ago"
+    data[:text_description] = object.text_description
+    if ['property_posted', 'request_for_termination', 'property_bidding_ended'].include?(object.act_type)
+      data[:property] = UserPropertySerializer.new(object.resource)
+    elsif ['bid_submission','offer_submission','buy_now_submission'].include?(object.act_type)
+      data[:property] = UserPropertySerializer.new(object.resource.property)
+    end
     data
   end
 end
