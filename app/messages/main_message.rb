836b7bb0ef018bcdcc1ba085e71705ecc54d0bec
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-13 16:10:49 +0530

    messages main parent class added

---
+++ app/messages/main_message.rb
@@ -0,0 +1,23 @@
+class MainMessage
+  private
+  def self.manage_body(message, user=nil, property=nil)
+    # puts message
+    msg = message
+    if message.include?("{user_verification_code}")
+      if user
+        msg = msg.gsub("{user_verification_code}", user.verification_code)
+      end
+    end
+    if message.include?("{password_reset_path}")
+      if user
+        msg = msg.gsub("{password_reset_path}", "#{APP_CONFIG['site_url']}/new_password?reset_token=#{user.auth_token}")
+      end
+    end
+    if message.include?("{property_address}")
+      if property
+        msg = msg.gsub("{property_address}", property.address)
+      end
+    end
+    return msg
+  end
+end
