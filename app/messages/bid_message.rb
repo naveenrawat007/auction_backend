deb2a47e94a3d1a0574d3bfd0e0c92c31e267a52
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-13 16:02:02 +0530

    bid message class methods changes templates taken from db

--- app/messages/bid_message.rb
+++ app/messages/bid_message.rb
@@ -1,7 +1,7 @@
-class BidMessage
-  def self.highest_bidder_notification(user, property)
+class BidMessage < MainMessage
+  def self.highest_bidder_notification(user, property) #code: "template19"
     begin
-      message = "Congratulations for being the Highest Bidder for #{property.address}!!!  You should be getting a message from the seller in regards to finalizing a contract or providing more information to them.  Check your messages in your profile and if you don’t hear from the seller within 24 hours please let us know? To Your Success, \n Your Support Team @ \n AuctionMyDeal.com "
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template19").body, user, property)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
