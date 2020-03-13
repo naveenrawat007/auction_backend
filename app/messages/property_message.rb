cda0becd0ea28a4f15715a4e0f453a91b96777c7
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-13 16:03:45 +0530

    property message class messages build up from db templates

--- app/messages/property_message.rb
+++ app/messages/property_message.rb
@@ -1,77 +1,77 @@
-class PropertyMessage
+class PropertyMessage < MainMessage
   ADMIN_NUMBERS = [7135531331, 9044900044]
-  def self.under_review(user, property)
+  def self.under_review(user, property) #code: "template4"
     begin
-      message = "Thank you for submitting your property at #{property.address}!  You will get an email and a message in your user dashboard that will advise you when your property is approved or if we have any questions. To Your Success, \n Your Support Team @ \n AuctionMyDeal.com "
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template4").body, user, property)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
   end
-  def self.best_offer(user, property)
+  def self.best_offer(user, property) #code: "template6"
     begin
-      message = "The property you submitted at #{property.address} has been approved! Check your email we just sent you that gives you"
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template6").body, user, property)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
   end
-  def self.live_bidding(user, property)
+  def self.live_bidding(user, property) #code: "template7"
     begin
-      message = "The property you submitted at #{property.address} is now active for Live Online Bidding status."
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template7").body, user, property)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
   end
-  def self.post_auction(user, property)
+  def self.post_auction(user, property) #code: "template8"
     begin
-      message = "The property you submitted at #{property.address} is now in post auction status so go to your user panel at AuctionMyDeal.com to accept, counter or relist your property under Best Offer or Live Online Auction?"
-      MessageSender.send("+1#{user.phone_number}", message)
-    rescue StandardError => e
-    end
-  end
-
-  def self.buy_now_notification(user, property)
-    begin
-      message = "Congratulations!!! You just got a buyer willing to pay your “Buy Now” price for your property at #{property.address}.  Your property is now in pending auction status until you go to your user panel at AuctionMyDeal.com to accept your “Buy Now” offer!  Your Support Team @ AuctionMyDeal.com"
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template8").body, user, property)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
   end
 
-  def self.sold_notification(user, property)
+  def self.buy_now_notification(user, property) #code: "template9"
     begin
-      message = "Congratulations on selling your property at #{property.address}!!!  Our goal is to go the extra yard for our members and we would like to learn from every property that’s posted on our site so that we can learn from our successes and failures.  It would mean a lot if you would take just a minute or 2 to give us your honest feedback? \n Your Support Team @ \n AuctionMyDeal.com "
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template9").body, user, property)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
   end
 
-  def self.chat_notification(user)
+  def self.sold_notification(user, property) #code: "template10"
     begin
-      message = "You have received a message at AuctionMyDeal.com."
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template10").body, user, property)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
   end
 
-  def self.buyer_best_offer_notification(user, property)
+  def self.chat_notification(user) #code: "template24"
     begin
-      message = "Thank you for your Best Offer on #{property.address} at AuctionMyDeal.com.  The seller is considering your offer and will be letting you know if your offer is acceptable or not.  They have the option to accept, reject or counter your offer, so look for a message from them in your messages under your profile?"
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template24").body, user)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
   end
 
-  def self.watchers_post_notification(user, property)
+  def self.buyer_best_offer_notification(user, property) #code: "template13"
     begin
-      message = "The property at #{property.address} is now pending or off the market, but we will let you know if it comes back on the market.  Your Support Team @ AuctionMyDeal.com"
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template13").body, user, property)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
   end
 
-  def self.termination_notification(user, property)
+  def self.watchers_post_notification(user, property) #code: "template20"
     begin
-      message = "We noticed that your terminated your property at #{property.address} so let us know if there’s anything we can do to help you?  Your Support Team @ AuctionMyDeal.com…:)"
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template20").body, user, property)
+      MessageSender.send("+1#{user.phone_number}", message)
+    rescue StandardError => e
+    end
+  end
+
+  def self.termination_notification(user, property) #code: "template12"
+    begin
+      message = self.manage_body(NotificationMessageTemplate.find_by(code: "template12").body, user, property)
       MessageSender.send("+1#{user.phone_number}", message)
     rescue StandardError => e
     end
