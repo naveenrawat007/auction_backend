7780a2270223433931e5e1100007ab0bf74c53ba
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-13 16:01:26 +0530

    confirmation sender code shitfted

--- app/lib/confirmation_sender.rb
+++ app/lib/confirmation_sender.rb
@@ -4,8 +4,6 @@ class ConfirmationSender
     user.update(verification_code: code)
     begin
       Sidekiq::Client.enqueue_to_in("default", Time.now, UserVerificationWorker, user.id)
-      verification_code = "Please enter the following verification code to activate your account at www.AuctionMyDeal.com.  Verification Code: #{user.verification_code} "
-      MessageSender.send_code("+1#{user.phone_number}", verification_code)
     rescue StandardError => e
     end
   end
