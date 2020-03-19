23f18eb1a5e078c3dbb8da023dd3fc7f6b800464
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-19 14:11:55 +0530

    admin controller property service used

--- app/controllers/api/v1/admin/properties_controller.rb
+++ app/controllers/api/v1/admin/properties_controller.rb
@@ -63,17 +63,16 @@ module Api
                   @property.status = "Approve"
                   @property.auction_started_at = params[:property][:auction_started_at]
                   @property.auction_length = params[:property][:auction_length]
-                  @property = set_property_timing(@property)
+                  @property = PropertyTimeService.new(@property).set_property_timing
                   @property.save
                 end
                 if params[:property][:best_offer] == "true"
                   @property.best_offer = true
                   @property.best_offer_auction_started_at = params[:property][:best_offer_auction_started_at]
                   @property.best_offer_auction_ending_at = params[:property][:best_offer_auction_ending_at]
-                  @property = set_property_timing(@property)
+                  @property = PropertyTimeService.new(@property).set_property_timing
                   @property.save
                 end
-                if @property.auction_started_at.blank? == false
                   if @property.best_offer == true
                     if @property.best_offer_auction_started_at.blank? == false
                       best_offer_live_auction(@property)
@@ -82,6 +81,7 @@ module Api
                       best_offer_post_auction(@property)
                     end
                   end
+                if @property.auction_started_at.blank? == false
                   live_auction(@property)
                   post_auction(@property)
                   @property.save
@@ -151,35 +151,6 @@ module Api
           else
             render json: {message: "Property not found", status: 404 }, status: 200
           end
-        end
-        private
-        def set_property_timing(property)
-          if property.best_offer == true
-            if property.best_offer_auction_started_at.blank? == false
-              if property.best_offer_auction_started_at.to_i != (property.best_offer_auction_started_at.beginning_of_day + 8.hours).to_i
-                property.best_offer_auction_started_at = property.best_offer_auction_started_at.beginning_of_day + 8.hours
-              end
-            end
-            if property.best_offer_auction_ending_at.blank? == false
-              if property.best_offer_auction_ending_at.to_i != (property.best_offer_auction_ending_at.end_of_day - 4.hours).to_i
-                property.best_offer_auction_ending_at = property.best_offer_auction_ending_at.end_of_day - 4.hours
-                property.auction_started_at = (property.best_offer_auction_ending_at + 1.day).beginning_of_day + 8.hours
-                property.auction_bidding_ending_at = (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
-              end
-            end
-          end
-          if property.auction_started_at.blank? == false
-            if property.auction_started_at.to_i != (property.auction_started_at.beginning_of_day + 8.hours).to_i
-              property.auction_started_at = property.auction_started_at.beginning_of_day + 8.hours
-              property.auction_bidding_ending_at = (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
-            end
-          end
-          if property.auction_bidding_ending_at == false
-            if property.auction_bidding_ending_at.to_i != (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
-              property.auction_bidding_ending_at = (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
-            end
-          end
-          return property
         end
       end
     end
