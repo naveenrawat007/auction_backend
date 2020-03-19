12db7a08dc168aeb1367299d1cc7b965ade44089
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-19 16:29:37 +0530

    properties controller resgister action fixes

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -204,6 +204,8 @@ module Api
             if @user.save
               CreateActivityService.new(@user, "user_register").process!
               token = JsonWebToken.encode(user_id: @user.id)
+              @user.status = "Free"
+              @user.trial_ending_at = Time.now.beginning_of_day + 60.days
               @user.auth_token = token
               @user.save
             else
@@ -230,85 +232,9 @@ module Api
         if @user
           @property = @user.owned_properties.new(property_update_params)
           @property.status = "Draft"
-          if params[:property][:residential_attributes].blank? == false
-            @property.residential_attributes = residential_type_attributes_permitter
-            @property.save
-          end
-          if params[:property][:commercial_attributes].blank? == false
-            @property.commercial_attributes = commercial_type_attributes_permitter
-            @property.save
-          end
-          if params[:property][:land_attributes].blank? == false
-            @property.land_attributes = land_type_attributes_permitter
-            @property.save
-          end
-          @property.estimated_rehab_cost_attr = estimated_rehab_cost_attributes_permitter
-          @property.buy_option = buy_option_permitter
-          if @property.best_offer == true
-            if @property.best_offer_auction_started_at.blank? == false
-              if @property.best_offer_auction_started_at.to_i != (@property.best_offer_auction_started_at.beginning_of_day + 8.hours).to_i
-                @property.best_offer_auction_started_at = @property.best_offer_auction_started_at.beginning_of_day + 8.hours
-              end
-            end
-            if @property.best_offer_auction_ending_at.blank? == false
-              if @property.best_offer_auction_ending_at.to_i != (@property.best_offer_auction_ending_at.end_of_day - 4.hours).to_i
-                @property.best_offer_auction_ending_at = @property.best_offer_auction_ending_at.end_of_day - 4.hours
-                @property.auction_started_at = (@property.best_offer_auction_ending_at + 1.day).beginning_of_day + 8.hours
-                @property.auction_bidding_ending_at = (@property.auction_started_at + @property.auction_length.to_i.days).beginning_of_day - 4.hours
-              end
-            end
-          end
-          if @property.auction_started_at.blank? == false
-            if @property.auction_started_at.to_i != (@property.auction_started_at.beginning_of_day + 8.hours).to_i
-              @property.auction_started_at = @property.auction_started_at.beginning_of_day + 8.hours
-              @property.auction_bidding_ending_at = (@property.auction_started_at + @property.auction_length.to_i.days).beginning_of_day - 4.hours
-            end
-          end
-          if @property.auction_ending_at.blank? == false
-            if @property.auction_ending_at.to_i != @property.auction_ending_at.end_of_day.to_i
-              @property.auction_ending_at = @property.auction_ending_at.end_of_day
-            end
-          end
-
-          if params[:images].blank? == false
-            @property.photos.destroy_all
-            params[:images].each do |image|
-              @property.photos.create(image: image)
-            end
-          end
-          if params[:video].blank? == false
-            @property.videos.destroy_all
-            @property.videos.create(video: params[:video])
-          end
-          if params[:arv_proof].blank? == false
-            @property.arv_proofs.destroy_all
-            @property.arv_proofs.create(file: params[:arv_proof], name: "Arv Proof")
-          end
-          if params[:rehab_cost_proof].blank? == false
-            @property.rehab_cost_proofs.destroy_all
-            @property.rehab_cost_proofs.create(file: params[:rehab_cost_proof], name: "Rehab Cost Proof")
-          end
-          if params[:rental_proof].blank? == false
-            @property.rental_proofs.destroy_all
-            @property.rental_proofs.create(file: params[:rental_proof], name: "Rental Proof")
-          end
-          if open_house_dates_permitter.blank? == false
-            @property.open_house_dates = open_house_dates_permitter
-          end
           @property.save
-          if @property.deal_analysis_type == "Rehab & Flip Deal"
-            @property.profit_potential = params[:property][:profit_potential]
-          elsif @property.deal_analysis_type == "Landlord Deal"
-            if @property.landlord_deal
-              @landlord_deal = @property.landlord_deal
-            else
-              @landlord_deal = @property.build_landlord_deal
-              @landlord_deal.save
-            end
-            @landlord_deal.update(landlord_deal_params)
-            @landlord_deal.save
-          end
-          if @property.save
+          result = PropertyUpdateService.new(@property, params, @user).user_process!
+          if result.status == "success"
             render json: {property: PropertySerializer.new(@property), user_token: @user.auth_token, user: UserSerializer.new(@user, root: false, serializer_options: {token: @user.auth_token}), message: "Property added sucessfully.", status: 201}, status: 200
           else
             render json: {message: "Property could not be added.", status: 400}, status: 200
