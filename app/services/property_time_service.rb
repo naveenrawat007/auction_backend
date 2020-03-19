7be76e967a09da115fa0c83702cca47379b0024a
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-19 14:12:56 +0530

    property time service added

---
+++ app/services/property_time_service.rb
@@ -0,0 +1,80 @@
+class PropertyTimeService
+  include ApplicationHelper
+  attr_reader :property
+
+  def initialize(property)
+    @property = property
+ 	end
+
+  def set_property_timing_admin(property)
+    if property.best_offer == true
+      if property.best_offer_auction_started_at.blank? == false
+        if property.best_offer_auction_started_at.to_i != (property.best_offer_auction_started_at.beginning_of_day + 8.hours).to_i
+          property.best_offer_auction_started_at = property.best_offer_auction_started_at.beginning_of_day + 8.hours
+          best_offer_live_auction(property)
+        end
+      end
+      if property.best_offer_auction_ending_at.blank? == false
+        if property.best_offer_auction_ending_at.to_i != (property.best_offer_auction_ending_at.end_of_day - 4.hours).to_i
+          property.best_offer_auction_ending_at = property.best_offer_auction_ending_at.end_of_day - 4.hours
+          property.auction_started_at = (property.best_offer_auction_ending_at + 1.day).beginning_of_day + 8.hours
+          property.auction_bidding_ending_at = (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
+          best_offer_post_auction(property)
+        end
+      end
+    end
+    if property.auction_started_at.blank? == false
+      if property.auction_started_at.to_i != (property.auction_started_at.beginning_of_day + 8.hours).to_i
+        property.auction_started_at = property.auction_started_at.beginning_of_day + 8.hours
+        property.auction_bidding_ending_at = (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
+      end
+    end
+    if property.auction_bidding_ending_at == false || property.auction_length.blank? == false
+      if property.auction_bidding_ending_at.to_i != ((property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours).to_i
+        property.auction_bidding_ending_at = (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
+      end
+    end
+    if property.auction_ending_at.blank? == false
+      if property.auction_ending_at.to_i != property.auction_ending_at.end_of_day.to_i
+        property.auction_ending_at = property.auction_ending_at.end_of_day
+      end
+    end
+    live_auction(property)
+    post_auction(property)
+    return property
+  end
+
+  def set_property_timing
+    if property.best_offer == true
+      if property.best_offer_auction_started_at.blank? == false
+        if property.best_offer_auction_started_at.to_i != (property.best_offer_auction_started_at.beginning_of_day + 8.hours).to_i
+          property.best_offer_auction_started_at = property.best_offer_auction_started_at.beginning_of_day + 8.hours
+        end
+      end
+      if property.best_offer_auction_ending_at.blank? == false
+        if property.best_offer_auction_ending_at.to_i != (property.best_offer_auction_ending_at.end_of_day - 4.hours).to_i
+          property.best_offer_auction_ending_at = property.best_offer_auction_ending_at.end_of_day - 4.hours
+          property.auction_started_at = (property.best_offer_auction_ending_at + 1.day).beginning_of_day + 8.hours
+          property.auction_bidding_ending_at = (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
+        end
+      end
+    end
+    if property.auction_started_at.blank? == false
+      if property.auction_started_at.to_i != (property.auction_started_at.beginning_of_day + 8.hours).to_i
+        property.auction_started_at = property.auction_started_at.beginning_of_day + 8.hours
+        property.auction_bidding_ending_at = (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
+      end
+    end
+    if property.auction_bidding_ending_at == false || property.auction_length.blank? == false
+      if property.auction_bidding_ending_at.to_i != ((property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours).to_i
+        property.auction_bidding_ending_at = (property.auction_started_at + property.auction_length.to_i.days).beginning_of_day - 4.hours
+      end
+    end
+    if property.auction_ending_at.blank? == false
+      if property.auction_ending_at.to_i != property.auction_ending_at.end_of_day.to_i
+        property.auction_ending_at = property.auction_ending_at.end_of_day
+      end
+    end
+    return property
+  end
+end
