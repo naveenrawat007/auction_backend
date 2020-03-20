d6c28b2ed477703cb937f24d9661076f0cd67ea6
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-20 16:26:22 +0530

    best offer mailer modifications

--- app/mailers/best_offer_mailer.rb
+++ app/mailers/best_offer_mailer.rb
@@ -1,30 +1,34 @@
 class BestOfferMailer < ApplicationMailer
   prepend_view_path NotificationMailerTemplate.resolver
-  def not_accepted(offer_id, test_email=nil)#code: "template14"
+  def not_accepted(offer_id, test_email=nil, user_id=nil, property_id=nil)#code: "template14"
     @offer = BestOffer.find_by(id: offer_id)
     if @offer
-  	  @buyer = User.find_by(id: @offer.user_id)
+      @buyer = User.find_by(id: @offer.user_id)
       @property = Property.find_by(id: @offer.property_id)
       if @buyer && @property
-        if test_email
-          mail(to: [test_email], subject: "Are you still interested in buying #{@property.address}???.")
-        else
-          mail(to: [@buyer.email], subject: "Are you still interested in buying #{@property.address}???.")
-        end
+        mail(to: [@buyer.email], subject: "Are you still interested in buying #{@property.address}???.")
+      end
+    else
+      @buyer = User.find_by(id: user_id)
+      @property = Property.find_by(id: property_id)
+      if @buyer && @property
+        mail(to: [test_email], subject: "Are you still interested in buying #{@property.address}???.")
       end
     end
 	end
-  def out_bidded(offer_id, test_email=nil)#template15
+  def out_bidded(offer_id, test_email=nil, user_id=nil, property_id=nil)#template15
     @offer = BestOffer.find_by(id: offer_id)
     if @offer
   	  @buyer = User.find_by(id: @offer.user_id)
       @property = Property.find_by(id: @offer.property_id)
       if @buyer && @property
-        if test_email
-          mail(to: [test_email], subject: "#{@property.address} is now Pending.")
-        else
-          mail(to: [@buyer.email], subject: "#{@property.address} is now Pending.")
-        end
+        mail(to: [@buyer.email], subject: "#{@property.address} is now Pending.")
+      end
+    else
+      @buyer = User.find_by(id: user_id)
+      @property = Property.find_by(id: property_id)
+      if @buyer && @property
+        mail(to: [test_email], subject: "#{@property.address} is now Pending.")
       end
     end
 	end
