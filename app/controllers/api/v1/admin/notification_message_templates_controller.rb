c80aaae6a7d351acf710873904148f398a36bbd2
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-23 18:24:37 +0530

    send test message action added

--- app/controllers/api/v1/admin/notification_message_templates_controller.rb
+++ app/controllers/api/v1/admin/notification_message_templates_controller.rb
@@ -22,6 +22,20 @@ module Api
           end
         end
 
+        def send_test_message
+          message_template = NotificationMessageTemplate.find_by(id: params[:id])
+          if message_template
+            if params[:number].blank? == false
+              Sidekiq::Client.enqueue_to_in("default", Time.now, TestMessageSendOutWorker, message_template.id, params[:number])
+              render json: {message: "Message has been sent.", status: 200}, status: 200
+            else
+              render json: {message: "Number is blank.", status: 400}, status: 200
+            end
+          else
+            render json: {message: "Message Template is not found.", status: 400}, status: 200
+          end
+        end
+
         private
         def template_params
           params.require(:template).permit(:body, :title)
