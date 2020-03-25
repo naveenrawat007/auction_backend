080bd9631af1926462a3e69ffc4ac04a35f03f7c
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-25 21:38:13 +0530

    image orientation issue fix

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-  has_attached_file :image, styles: { thumb: ["291x140#", :jpg] }
+  has_attached_file :image, styles: { thumb: ["291x140#", :jpg], original: {convert_options: '-auto-orient'} }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   belongs_to :imageable, polymorphic: true
 end
