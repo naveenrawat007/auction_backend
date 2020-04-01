466000d6789e74b5275f0f4e45be3a67dd4371f6
Vinay Mehta <vinay.ymca@gmail.com>
2020-04-01 09:13:59 +0530

    apply polymorphic association

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,6 +3,8 @@ class Bid < ApplicationRecord
   belongs_to :user
   has_many :fund_proofs, as: :resource, class_name: "Attachment", dependent: :destroy
   has_one :chat_room, as: :offer, dependent: :destroy
+  has_one :offer_detail, as: :offer, dependent: :destroy
+
 
   def get_fund_proof
     if self.fund_proofs.blank? == false
--- app/models/buy_now_offer.rb
+++ app/models/buy_now_offer.rb
@@ -3,6 +3,7 @@ class BuyNowOffer < ApplicationRecord
   belongs_to :user
   has_many :fund_proofs, as: :resource, class_name: "Attachment", dependent: :destroy
   has_one :chat_room, as: :offer, dependent: :destroy
+  has_one :offer_detail, as: :offer, dependent: :destroy
 
   def get_fund_proof
     if self.fund_proofs.blank? == false
