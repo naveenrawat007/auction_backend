374d155865cbeca652001b042e11b0b46adb4281
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-04-04 18:03:58 +0530

    attachments added in offer details modal

--- app/models/offer_detail.rb
+++ app/models/offer_detail.rb
@@ -1,3 +1,4 @@
 class OfferDetail < ApplicationRecord
   belongs_to :offer, polymorphic: true, optional: true
+  has_many :business_documents, as: :resource, class_name: "Attachment", dependent: :destroy
 end
