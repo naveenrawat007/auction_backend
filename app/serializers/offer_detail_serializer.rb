0eeedfb588882cd4454eb767d602e8e930fefb48
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-04-04 23:37:02 +0530

    offer and offer detail serializer added

---
+++ app/serializers/offer_detail_serializer.rb
@@ -0,0 +1,26 @@
+class OfferDetailSerializer < ActiveModel::Serializer
+  def attributes(*args)
+    data = super
+    data[:id] = object.id
+    data[:user_first_name] = object.user_first_name
+    data[:user_middle_name] = object.user_middle_name
+    data[:user_last_name] = object.user_last_name
+    data[:user_email] = object.user_email
+    data[:user_phone_no] = object.user_phone_no
+    data[:self_buy_property] = object.self_buy_property
+    data[:realtor_first_name] = object.realtor_first_name
+    data[:realtor_last_name] = object.realtor_last_name
+    data[:realtor_license] = object.realtor_license
+    data[:realtor_company] = object.realtor_company
+    data[:realtor_phone_no] = object.realtor_phone_no
+    data[:realtor_email] = object.realtor_email
+    data[:purchase_property_as] = object.purchase_property_as
+    data[:internet_transaction_fee] = object.internet_transaction_fee
+    data[:total_due] = object.total_due
+    data[:promo_code] = object.promo_code
+    data[:property_closing_date] = object.property_closing_date
+    data[:hold_bid_days] = object.hold_bid_days
+    data[:business_document_text] = object.business_document_text
+    data
+  end
+end
---
+++ app/serializers/offer_serializer.rb
@@ -0,0 +1,38 @@
+class OfferSerializer < ActiveModel::Serializer
+  def attributes(*args)
+    data = super
+    data[:id] = object.id
+    data[:amount] = object.amount
+    data[:type] = check_type
+    data[:user] = object.user ? object.user.first_name : ""
+    data[:user_name] = object.user ? "#{object.user.first_name} #{object.user.last_name}" : ""
+    data[:time] = object.updated_at.strftime("%m/%d/%Y | %l:%M%p")
+    data[:fund_proof] = object.get_fund_proof
+    data[:user_type] = user_type
+    data[:accepted] = object.accepted
+    if object.offer_detail
+      data[:offer_detail] = OfferDetailSerializer.new(object.offer_detail)
+    end
+    data
+  end
+  def user_type
+    u_type = ""
+    if object.user.type_attributes.blank? == false
+      object.user.type_attributes.each do |type|
+        if type[1] == true
+          u_type = type[0]
+          break
+        end
+      end
+    end
+    if u_type.include?("Inves")
+      "Investor"
+    else
+      "Realtor"
+    end
+  end
+
+  def check_type
+    object.class.to_s
+  end
+end
