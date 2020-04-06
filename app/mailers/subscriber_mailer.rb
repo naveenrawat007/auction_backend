70c01a15e3fbaf05c273ac271ad436ba66f52c37
Vinay Mehta <vinay.ymca@gmail.com>
2020-04-06 17:16:49 +0530

    set path for dummy pdf

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -2,7 +2,7 @@ class SubscriberMailer < ApplicationMailer
 
   def auction_guide(email, name)
     @name = name
-    attachments['auction_guide.pdf'] = File.read('path/to/auction_guide.pdf')
+    attachments['auction_guide.pdf'] = File.read("#{Rails.root}/app/assets/dummy.pdf")
     mail(to: email, subject: "Auction Guide")
 	end
 
