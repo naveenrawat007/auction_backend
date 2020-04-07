c83f01659f4d9b150cf293d4f22533aef74fc92a
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-04-07 21:46:31 +0530

    call back added to accept offer

--- app/models/buy_now_offer.rb
+++ app/models/buy_now_offer.rb
@@ -5,6 +5,13 @@ class BuyNowOffer < ApplicationRecord
   has_one :chat_room, as: :offer, dependent: :destroy
   has_one :offer_detail, as: :offer, dependent: :destroy
 
+  after_create :accept_offer
+
+  def accept_offer
+    self.accepted = true
+    self.save
+  end
+
   def get_fund_proof
     if self.fund_proofs.blank? == false
       APP_CONFIG['backend_site_url'] + self.fund_proofs.first.file.url
