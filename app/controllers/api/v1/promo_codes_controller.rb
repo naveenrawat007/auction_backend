82c60a89004bd003fa77e700dc19b56a3888e479
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-04-16 23:49:55 +0530

    promocode controller changes

--- app/controllers/api/v1/promo_codes_controller.rb
+++ app/controllers/api/v1/promo_codes_controller.rb
@@ -4,16 +4,24 @@ module Api
       before_action :authorize_request
 
       def apply_code
-        code = PromoCode.find_by(promo_code: params[:promo_code][:promo_code])
+        code = PromoCode.find_by(promo_code: params[:promo_code])
         if code
-          render json: {status: 200, message: "Promo Code Applied"}
-        else
-          if !@current_user.promo_code.present?
-            render json: {status: 200, message: "Promo Code Applied"}
-          else
-            render json: {status: 400, message: "Promo Code already applied"}
+          if (code.user.id == @current_user.id) || code.user.is_admin
+            render json: {status: 200, message: "Promo Code Applied"}, status: 200  and return
           end
         end
+        render json: {status: 400, message: "Promo Code is not valid."}, status: 200
+      end
+      def show
+        if @current_user.promo_code
+          promo_code = @current_user.promo_code
+          if promo_code.availed
+            render json: {status: 400, message: "Promo Code already applied"}, status: 200 and return
+          end
+        else
+          promo_code = PromoCodeService.new(@current_user).generated_code!
+        end
+        render json: {message: "Promo Code", promo_code: promo_code.promo_code, status: 200}, status: 200
       end
 
       private
